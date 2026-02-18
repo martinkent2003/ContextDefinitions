13ef3c17e959380e52565f61fb2cbbd93667a3bc
Alex Vargas <alex48vargas@gmail.com>
2026-02-17 22:58:02 -0500

    process reading refactor

---
+++ shared/reading-structure/v1.ts
@@ -0,0 +1,43 @@
+export interface ReadingStructureV1 {
+  schema: "reading_structure_v1";
+  reading_id: string;
+  language_code: string;
+  text: {
+    storage_path: string;
+    format: "text/plain";
+    offset_unit: "codepoint";
+  };
+  processor: {
+    tokenizer: string;
+    tokenizer_version: string;
+    phrase_rules_version: string;
+    generated_at: string;
+  };
+  blocks: {
+    i: number;
+    type: "paragraph";
+    start: number;
+    end: number;
+  }[];
+  sentences: {
+    i: number;
+    start: number;
+    end: number;
+  }[];
+  tokens: {
+    i: number;
+    start: number;
+    end: number;
+    surface: string;
+    norm: string;
+    kind: "word" | "number" | "punct" | "symbol" | "other";
+  }[];
+  spans: {
+    id: string;
+    type: "phrase" | "contraction";
+    start: number;
+    end: number;
+    surface: string;
+    token_range: [number, number];
+  }[];
+}
